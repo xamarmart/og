a9da72cc82ce906b17318603bc50cb8c28ae6542
xamarmart <xamarmart@gmail.com>
2026-01-17 02:44:07 +0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
+import App from "./App";
+import ErrorBoundary from './components/ErrorBoundary';
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
